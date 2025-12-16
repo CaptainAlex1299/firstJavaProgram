a978f07966d73a7f1acd90f225117f52b89c4d85
Alexander <alexanderdietstein@gmail.com>
2025-12-16 10:22:07 +0200

    deleted redundant files

--- src/TempConverter.java
+++
@@ -1,9 +0,0 @@
-public class TempConverter {
-    public static double toCelsius(double number) {
-        return (number - 32) * (5.0 / 9.0);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(TempConverter.toCelsius(44));
-    }
-}
